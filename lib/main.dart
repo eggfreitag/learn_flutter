076ce216ddb9adccb0da7fcae996d415fb7c1e30
Wonhee Kang <eggfreitag@gmail.com>
2023-05-02 18:19:06 +0900

    스타일링 클론코딩 해보기

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,17 +14,34 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("My App"),
+          backgroundColor: Color(Colors.white.value),
           centerTitle: false,
-          leading: Icon(Icons.menu),
+          leading: Center(
+            child: Text(
+              "금호동 3가",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          leadingWidth: 100,
           actions: [
             IconButton(
               icon: Icon(Icons.search),
               onPressed: () => print("검색"),
+              color: Colors.black,
             ),
             IconButton(
-              icon: Icon(Icons.home),
-              onPressed: () => print("홈"),
+              icon: Icon(Icons.menu),
+              onPressed: () => print("Menu"),
+              color: Colors.black,
+            ),
+            IconButton(
+              icon: Icon(Icons.notifications),
+              onPressed: () => print("Notifications"),
+              color: Colors.black,
             ),
           ],
         ),
@@ -30,17 +49,54 @@ class MyApp extends StatelessWidget {
         //   child: Text("하이"),
         //   onPressed: () => print("하이"),
         // ),
-        body: ElevatedButton(
-          onPressed: () => print("하이"),
-          style: ButtonStyle(
-              alignment: Alignment.bottomCenter,
-              backgroundColor: MaterialStateProperty.all(Colors.red)),
-          child: Text("하이"),
-        ),
+        // body: ElevatedButton(
+        //   onPressed: () => print("하이"),
+        //   style: ButtonStyle(
+        //       alignment: Alignment.bottomCenter,
+        //       backgroundColor: MaterialStateProperty.all(Colors.red)),
+        //   child: Text("하이"),
+        // ),
         // body: IconButton(
         //   icon: Icon(Icons.home),
         //   onPressed: () => print("하이"),
         // ),
+        body: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 150,
+              height: 150,
+              color: Colors.red,
+              margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+            ),
+            Expanded(
+              child: SizedBox(
+                height: 150,
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(0, 10, 10, 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "캐논 DSLR 100D (단렌즈, 충전기 16기가 SD 포함)",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      Text("성동구 행당동 - 끌올 10분 전",
+                          style: TextStyle(fontSize: 15)),
+                      Text("100,000원", style: TextStyle(fontSize: 15)),
+                      Container(
+                        alignment: Alignment.centerRight,
+                        child: Icon(Icons.dark_mode_outlined),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
