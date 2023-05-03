e7b58765a51e14da370ea092eb8a8eaf25275798
Wonhee Kang <eggfreitag@gmail.com>
2023-05-03 13:40:58 +0900

    커스텀 위젯 적용

--- lib/main.dart
+++ lib/main.dart
@@ -6,57 +6,47 @@ void main() {
   runApp(const MyApp());
 }
 
+var sizedBox = SizedBox(
+  child: Text("안녕"),
+);
+// 변수에 할당하여 사용
+// 성능상 issue 야기할 가능성 있다 (? 변화하는 값을 포함할 경우)
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        body: Container(
-          height: 150,
-          padding: EdgeInsets.all(10),
-          child: Row(
-            children: [
-              Image.asset(
-                'camera.jpeg',
-                width: 150,
-                height: 150,
-              ),
-              Flexible(
-                flex: 1,
-                child: Container(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "카메라",
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      Text(
-                        "금호동",
-                        style: TextStyle(fontSize: 15),
-                      ),
-                      Text(
-                        "7천원",
-                        style: TextStyle(fontSize: 15),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Icon(Icons.favorite),
-                          Text("4"),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+        body: ListView(
+          children: [
+            Text("안녕"),
+          ],
         ),
+        // body: sizedBox,
       ),
     );
+  }
+}
+
+class ShopItem extends StatelessWidget {
+  // 커스텀 위젯은 class로 생성된다
+  // StatelessWidget클래스에서 상속받아 사용한다
+  const ShopItem({super.key});
+  // parameter정의
+
+  @override
+  // class에서 상속받은 변수, 함수의 중복을 재 정의한다
+  // 예를들어 아래의 build 함수를 StatelessWidget의 build함수가 아닌
+  // 아래에 정의한 build 함수로 사용하라는 의미
+  Widget build(BuildContext context) {
+    // Widget, BuildContext는 type을 지정
+    // 결국은 build(context)라는 함수이다
+    return SizedBox(
+      child: Text("안녕"),
+    );
+    // return안에 레이아웃을 정의한다
   }
 }
